1ded85faa7e5fe505d977805c4ab9570347a7e55
Aritra <chatterjee.aritra@protonmail.com>
2023-06-24 16:03:39 +0530

    userList

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 
 
@@ -12,14 +12,10 @@ function App() {
       <Topbar/>
       <div className="container">
         <Sidebar/>
-        <Switch>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-          <Route path="/users">
-            <UserList/>
-          </Route>
-        </Switch>
+        <Routes>
+          <Route exact path="/" element= <Home/> />
+          <Route path="/users" element=<UserList/> />
+        </Routes>
       </div>
     </Router>
   );
